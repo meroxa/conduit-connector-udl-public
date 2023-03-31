cf6e9bace97138c43812f6e488dc92390b85735e
William Hill <william@meroxa.io>
2023-03-31 18:02:21 -0400

    feat: successful test with sending test with sending AIS data to UDL

--- destination.go
+++ destination.go
@@ -2,8 +2,13 @@ package connector
 
 import (
 	"context"
+
+	"encoding/json"
+	"fmt"
+
 	sdk "github.com/conduitio/conduit-connector-sdk"
 	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
+
 	"github.com/meroxa/conduit-connector-udl/udl"
 )
 
@@ -18,40 +23,44 @@ func NewDestination() sdk.Destination {
 }
 
 func (d *Destination) Parameters() map[string]sdk.Parameter {
-	return map[string]sdk.Parameter{
-		HTTPBasicAuthUsername: {
-			Default:     "",
-			Required:    true,
-			Description: "The HTTP Basic Auth Username to use when accessing the UDL.",
-		},
-		HTTPBasicAuthPassword: {
-			Default:     "",
-			Required:    true,
-			Description: "The HTTP Basic Auth Password to use when accessing the UDL.",
-		},
-		DataMode: {
-			Default:     "TEST",
-			Required:    false,
-			Description: "The Data Mode to use when submitting requests to the UDL. Acceptable values are REAL, TEST, SIMULATED and EXERCISE.",
-		},
-		BaseURL: {
-			Default:     "https://unifieddatalibrary.com",
-			Required:    false,
-			Description: "The Base URL to use to access the UDL. The default is https://unifieddatalibrary.com.",
-		},
-		Endpoint: {
-			Default:     "",
-			Required:    true,
-			Description: "The target UDL endpoint.",
-		},
-	}
+	return d.config.Parameters()
+	// return map[string]sdk.Parameter{
+	// 	HTTPBasicAuthUsername: {
+	// 		Default:     "",
+	// 		Required:    true,
+	// 		Description: "The HTTP Basic Auth Username to use when accessing the UDL.",
+	// 	},
+	// 	HTTPBasicAuthPassword: {
+	// 		Default:     "",
+	// 		Required:    true,
+	// 		Description: "The HTTP Basic Auth Password to use when accessing the UDL.",
+	// 	},
+	// 	DataMode: {
+	// 		Default:     "TEST",
+	// 		Required:    false,
+	// 		Description: "The Data Mode to use when submitting requests to the UDL. Acceptable values are REAL, TEST, SIMULATED and EXERCISE.",
+	// 	},
+	// 	BaseURL: {
+	// 		Default:     "https://unifieddatalibrary.com",
+	// 		Required:    false,
+	// 		Description: "The Base URL to use to access the UDL. The default is https://unifieddatalibrary.com.",
+	// 	},
+	// 	Endpoint: {
+	// 		Default:     "",
+	// 		Required:    true,
+	// 		Description: "The target UDL endpoint.",
+	// 	},
+	// }
 }
 
 func (d *Destination) Configure(ctx context.Context, cfg map[string]string) error {
+	sdk.Logger(ctx).Debug().Msg("Configuring Destination connector...")
+	sdk.Logger(ctx).Debug().Msgf("config: %w", cfg)
 	parsedCfg, err := d.ParseDestinationConfig(cfg)
 	if err != nil {
 		return err
 	}
+	sdk.Logger(ctx).Debug().Msgf("parsedCfg: %w", parsedCfg)
 	d.config = parsedCfg
 	return nil
 }
@@ -62,6 +71,7 @@ func (d *Destination) Open(ctx context.Context) error {
 		return err
 	}
 	c, err := udl.NewClient(d.config.BaseURL, udl.WithRequestEditorFn(authProvider.Intercept))
+	sdk.Logger(ctx).Debug().Msgf("client url: %w", c.Client)
 	if err != nil {
 		return err
 	}
@@ -70,19 +80,40 @@ func (d *Destination) Open(ctx context.Context) error {
 }
 
 func (d *Destination) Write(ctx context.Context, records []sdk.Record) (int, error) {
-	var elsets []udl.ElsetIngest
+	// var elsets []udl.ElsetIngest
+	var aisData []udl.AISIngest
 	for _, r := range records {
-		elset, err := toUDLElset(r.Payload.After.Bytes())
+		sdk.Logger(ctx).Debug().Msgf("record: %w", r)
+		// elset, err := toUDLElset(r.Payload.After.Bytes())
+		// sdk.Logger(ctx).Debug().Msgf("elset: %w", elset)
+		ais, err := toUDLAis(r.Payload.After.Bytes())
+		sdk.Logger(ctx).Debug().Msgf("ais output: %+v", ais)
+		sdk.Logger(ctx).Debug().Msgf("ais ShipName: %s", ais.ShipName)
+		payload := make(sdk.StructuredData)
+		if err := json.Unmarshal(r.Payload.After.Bytes(), &payload); err != nil {
+			return 0, fmt.Errorf("unmarshal payload: %w", err)
+		}
+		sdk.Logger(ctx).Debug().Msgf("payload: %w", payload)
+		aisData = append(aisData, ais)
 		if err != nil {
 			return 0, err
 		}
-		elsets = append(elsets, elset)
+		// elsets = append(elsets, elset)
 	}
-	resp, err := d.client.FiledropUdlElsetPostId(ctx, elsets)
+
+	resp, err := d.client.FiledropUdlAisPostId(ctx, aisData)
+	sdk.Logger(ctx).Debug().Msgf("resp: %w", resp)
 	if err != nil || resp.StatusCode > 300 {
+		sdk.Logger(ctx).Debug().Msgf("err: %w", err)
 		return 0, err
 	}
-	return len(elsets), nil
+	sdk.Logger(ctx).Debug().Msgf("resp: %w", resp)
+
+	// resp, err := d.client.FiledropUdlElsetPostId(ctx, elsets)
+	// if err != nil || resp.StatusCode > 300 {
+	// 	return 0, err
+	// }
+	return len(aisData), nil
 }
 
 func generateBasicAuth(username, password string) (*securityprovider.SecurityProviderBasicAuth, error) {
--- transform.go
+++ transform.go
@@ -2,8 +2,54 @@ package connector
 
 import (
 	"encoding/json"
+
+	"fmt"
+
 	"github.com/meroxa/conduit-connector-udl/udl"
 )
+
+func flattenMap(inputMap map[string]interface{}, flattenedMap map[string]interface{}, parentKey string) {
+	for key, value := range inputMap {
+		newKey := key
+		if parentKey != "" {
+			newKey = parentKey + "." + key
+		}
+		switch v := value.(type) {
+		case map[string]interface{}:
+			flattenMap(v, flattenedMap, newKey)
+		default:
+			flattenedMap[newKey] = value
+		}
+	}
+}
+
+func toUDLAis(raw []byte) (udl.AISIngest, error) {
+	originalMap := make(map[string]interface{})
+	json.Unmarshal(raw, &originalMap)
+
+	flattenedMap := make(map[string]interface{})
+	flattenMap(originalMap, flattenedMap, "")
+	fmt.Println(fmt.Sprintf("flattenedMap: %s", flattenedMap))
+	fmt.Println(fmt.Sprintf("flattenedMap static data: %s", flattenedMap["staticData"]))
+	fmt.Println(fmt.Sprintf("flattenedMap static data name: %s", flattenedMap["staticData.name"]))
+
+	jsonData, _ := json.Marshal(flattenedMap)
+
+	var ais udl.AISIngest
+	// ais.Lat = flattenedMap["lastPositionUpdate.longitude"]
+	// ais.Lat = originalMap["lastPositionUpdate"]
+	// ais.ShipName = originalMap["staticData"]["name"]
+	// shipName := flattenedMap["staticData.name"].(string)
+	// ais.ShipName = &shipName
+	// err := json.Unmarshal(raw, &ais)
+
+	// TODO: Make these as part of config
+	error := json.Unmarshal(jsonData, &ais)
+	ais.DataMode = "TEST"
+	ais.Source = "Spire"
+	ais.ClassificationMarking = "U"
+	return ais, error
+}
 
 func toUDLElset(raw []byte) (udl.ElsetIngest, error) {
 	var elset udl.ElsetIngest
