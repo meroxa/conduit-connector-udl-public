5a441b2a78f5b123fa361ef2c18a844155ad5f79
William Hill <william@meroxa.io>
2023-04-15 12:36:14 -0400

    fix: updated struct and data types

--- vesselData.go
+++ vesselData.go
@@ -1,7 +1,5 @@
 package connector
 
-import "time"
-
 type Dimensions struct {
 	A      float64 `json:"a"`
 	B      float64 `json:"b"`
@@ -21,31 +19,47 @@ type StaticData struct {
 	Name            string     `json:"name"`
 	ShipSubType     string     `json:"shipSubType"`
 	ShipType        string     `json:"shipType"`
-	Timestamp       time.Time  `json:"timestamp"`
-	UpdateTimestamp time.Time  `json:"updateTimestamp"`
+	Timestamp       string     `json:"timestamp"`
+	UpdateTimestamp string     `json:"updateTimestamp"`
 }
 
 type LastPositionUpdate struct {
-	Accuracy           string    `json:"accuracy"`
-	CollectionType     string    `json:"collectionType"`
-	Course             float64   `json:"course"`
-	Heading            float64   `json:"heading"`
-	Latitude           float64   `json:"latitude"`
-	Longitude          float64   `json:"longitude"`
-	Maneuver           string    `json:"maneuver"`
-	NavigationalStatus string    `json:"navigationalStatus"`
-	ROT                int       `json:"rot"`
-	Speed              float64   `json:"speed"`
-	Timestamp          time.Time `json:"timestamp"`
-	UpdateTimestamp    time.Time `json:"updateTimestamp"`
+	Accuracy           string  `json:"accuracy"`
+	CollectionType     string  `json:"collectionType"`
+	Course             float64 `json:"course"`
+	Heading            float64 `json:"heading"`
+	Latitude           float64 `json:"latitude"`
+	Longitude          float64 `json:"longitude"`
+	Maneuver           string  `json:"maneuver"`
+	NavigationalStatus string  `json:"navigationalStatus"`
+	ROT                float64 `json:"rot"`
+	Speed              float64 `json:"speed"`
+	Timestamp          string  `json:"timestamp"`
+	UpdateTimestamp    string  `json:"updateTimestamp"`
+}
+
+type GeoPoint struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+type Port struct {
+	CenterPoint GeoPoint `json:"centerPoint"`
+	Name        string   `json:"name"`
+	Unlocode    string   `json:"unlocode"`
+}
+
+type MatchedPort struct {
+	MatchScore float64 `json:"matchScore"`
+	Port       Port    `json:"port"`
 }
 
 type CurrentVoyage struct {
-	Destination     string    `json:"destination"`
-	Draught         float64   `json:"draught"`
-	ETA             time.Time `json:"eta"`
-	Timestamp       time.Time `json:"timestamp"`
-	UpdateTimestamp time.Time `json:"updateTimestamp"`
+	Destination     string      `json:"destination"`
+	Draught         float64     `json:"draught"`
+	ETA             string      `json:"eta"`
+	MatchedPort     MatchedPort `json:"matchedPort"`
+	Timestamp       string      `json:"timestamp"`
+	UpdateTimestamp string      `json:"updateTimestamp"`
 }
 
 type VesselData struct {
@@ -53,5 +67,5 @@ type VesselData struct {
 	StaticData         StaticData         `json:"staticData"`
 	CurrentVoyage      CurrentVoyage      `json:"currentVoyage"`
 	LastPositionUpdate LastPositionUpdate `json:"lastPositionUpdate"`
-	UpdateTimestamp    time.Time          `json:"updateTimestamp"`
+	UpdateTimestamp    string             `json:"updateTimestamp"`
 }
