a89e716968ce665354e4cc19a4d67a5190d32e52
William Hill <william@meroxa.io>
2023-04-15 12:38:03 -0400

    feat: extracted code to write to UDL into functions

---
+++ udlWriter.go
@@ -0,0 +1,50 @@
+package connector
+
+import (
+	"context"
+
+	sdk "github.com/conduitio/conduit-connector-sdk"
+
+	"github.com/meroxa/conduit-connector-udl/udl"
+)
+
+func writeAisToUDL(ctx context.Context, records []sdk.Record, d *Destination) (int, error) {
+	var aisData []udl.AISIngest
+	for _, r := range records {
+		ais, err := toUDLAis(r.Payload.After.Bytes())
+		sdk.Logger(ctx).Debug().Msgf("ais output: %+v", ais)
+		aisData = append(aisData, ais)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	resp, err := d.client.FiledropUdlAisPostId(ctx, aisData)
+	if err != nil || resp.StatusCode > 300 {
+		sdk.Logger(ctx).Debug().Msgf("err: %w", err)
+		return 0, err
+	}
+	sdk.Logger(ctx).Debug().Msgf("resp: %w", resp)
+
+	return len(aisData), nil
+}
+
+func writeElsetToUDL(ctx context.Context, records []sdk.Record, d *Destination) (int, error) {
+	var elsets []udl.ElsetIngest
+	for _, r := range records {
+		sdk.Logger(ctx).Debug().Msgf("record: %w", r)
+		elset, err := toUDLElset(r.Payload.After.Bytes())
+		sdk.Logger(ctx).Debug().Msgf("elset: %w", elset)
+		if err != nil {
+			return 0, err
+		}
+		elsets = append(elsets, elset)
+	}
+
+	resp, err := d.client.FiledropUdlElsetPostId(ctx, elsets)
+	if err != nil || resp.StatusCode > 300 {
+		return 0, err
+	}
+
+	return len(elsets), nil
+}
